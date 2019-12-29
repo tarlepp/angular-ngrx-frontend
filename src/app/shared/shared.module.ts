b0c424c44a12bd3ba8d513f5f1191a0557d27efa
tarlepp <tarmo.leppanen@protacon.com>
2019-12-29 19:29:34 +0200

    feat: Added base structure for header component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { Components } from './components';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...Components,
+  ],
   imports: [
     BrowserAnimationsModule,
     CommonModule,

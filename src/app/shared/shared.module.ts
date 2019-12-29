05f39dde9fe6d4e82d0c8f46bd5fb25b8d773109
tarlepp <tarmo.leppanen@protacon.com>
2019-12-29 19:19:45 +0200

    feat: Added @angular/flex-layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserAnimationsModule,
     CommonModule,
+    FlexLayoutModule,
   ],
   exports: [
     BrowserAnimationsModule,
     CommonModule,
+    FlexLayoutModule,
   ],
 })
 

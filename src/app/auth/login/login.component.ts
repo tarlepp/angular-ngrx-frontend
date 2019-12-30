68196f62b9ac77514c1403f48c3362173bf9a718
tarlepp <tarmo.leppanen@protacon.com>
2019-12-30 21:37:09 +0200

    refactor: Added main login process to login component.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,25 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
+import { authenticationActions, authenticationSelectors, AuthenticationState } from '../../store/authentication';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   public loginForm: FormGroup;
+  public loading: boolean;
 
-  public constructor(private formBuilder: FormBuilder) { }
+  private subscriptions: Subscription;
+
+  public constructor(private formBuilder: FormBuilder, private authenticationStore: Store<AuthenticationState>) {
+    this.subscriptions = new Subscription();
+  }
 
   public ngOnInit(): void {
+    this.subscriptions
+      .add(this.authenticationStore
+        .pipe(select(authenticationSelectors.loading))
+        .subscribe((loading: boolean): void => {
+          this.loading = loading;
+        }),
+      );
+
     this.loginForm = this.formBuilder.group({
       username: [null, [Validators.required, Validators.minLength(3)]],
       password: [null, [Validators.required]],
     });
   }
 
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
   public submit(): void {
-    console.log(this.loginForm.value);
+    this.authenticationStore.dispatch(authenticationActions.login({credentials: this.loginForm.value}));
   }
 }

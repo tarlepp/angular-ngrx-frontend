de767ea40c56a6f3bd619ddf7becde81a4fc61d0
tarlepp <tarmo.leppanen@protacon.com>
2019-12-30 02:53:38 +0200

    fix: Added missing material and common modules.

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -5,11 +5,15 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialModules = [
   MatButtonModule,
   MatDividerModule,
+  MatFormFieldModule,
   MatIconModule,
+  MatInputModule,
   MatMenuModule,
   MatToolbarModule,
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -14,6 +15,7 @@ import { MaterialModule } from './material/material.module';
     BrowserAnimationsModule,
     CommonModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
     MaterialModule,
   ],
   exports: [
@@ -21,6 +23,7 @@ import { MaterialModule } from './material/material.module';
     BrowserAnimationsModule,
     CommonModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
     MaterialModule,
   ],
 })

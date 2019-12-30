c3c4473fd1b98a36584396b8484f88166bab4a1f
tarlepp <tarmo.leppanen@protacon.com>
2019-12-30 02:47:11 +0200

    feat: Implemented actual login form - no functionality to backend yet.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+
+  public constructor(private formBuilder: FormBuilder) { }
+
+  public ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [null, [Validators.required, Validators.minLength(3)]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  public submit(): void {
+    console.log(this.loginForm.value);
+  }
+}

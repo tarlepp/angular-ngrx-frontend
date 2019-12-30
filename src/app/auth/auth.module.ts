4cd63e7bec5f846d32b31db7f59ab77e68be47cd
tarlepp <tarmo.leppanen@protacon.com>
2019-12-30 21:26:47 +0200

    feat: Added JWT initialization to auth module.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
+import { JWT_OPTIONS, JwtModule } from '@auth0/angular-jwt';
+import { LocalStorageService, NgxWebstorageModule } from 'ngx-webstorage';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginModule } from './login/login.module';
+import { jwtOptionsFactory } from './factories';
+import { Guards } from './guards';
+import { Services } from './services';
 
 @NgModule({
   imports: [
     AuthRoutingModule,
     LoginModule,
+    NgxWebstorageModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory,
+        deps: [LocalStorageService],
+      },
+    }),
   ],
   providers: [
     ...Services,
---
+++ src/app/auth/factories/jwt-options.factory.ts
@@ -0,0 +1,16 @@
+import { JwtModuleOptions } from '@auth0/angular-jwt';
+import { LocalStorageService } from 'ngx-webstorage';
+
+import { ConfigurationService } from '../../shared/services';
+
+export function jwtOptionsFactory(localStorage: LocalStorageService): JwtModuleOptions['config'] {
+  return {
+    tokenGetter: (): any => localStorage.retrieve('token'),
+    whitelistedDomains: [
+      new URL(ConfigurationService.configuration.apiUrl).host,
+    ],
+    blacklistedRoutes: [
+      ConfigurationService.configuration.tokenUrl,
+    ],
+  };
+}

6dcf5d0b913de09f012c32c293f10dc257a9e652
tarlepp <tarmo.leppanen@protacon.com>
2019-12-31 21:21:11 +0200

    feat: Added selector for user profile to authentication store.

--- src/app/store/authentication/authentication.selectors.ts
+++ src/app/store/authentication/authentication.selectors.ts
@@ -2,14 +2,17 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import { AuthenticationState } from './authentication.state';
 import { ServerErrorInterface } from '../../shared/interfaces';
+import { UserProfileInterface } from '../../auth/interfaces';
 
 const authenticationState = createFeatureSelector<AuthenticationState>('authentication');
 const loading = createSelector(authenticationState, (state: AuthenticationState): boolean => state.loading);
 const loggedIn = createSelector(authenticationState, (state: AuthenticationState): boolean => state.loggedIn);
+const profile = createSelector(authenticationState, (state: AuthenticationState): UserProfileInterface => state.profile);
 const error = createSelector(authenticationState, (state: AuthenticationState): ServerErrorInterface|null => state.error);
 
 export const authenticationSelectors = {
   loading,
   loggedIn,
+  profile,
   error,
 };

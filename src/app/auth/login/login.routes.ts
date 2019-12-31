4438ed05dc92434ff4ed3afbdf9f167f506d4cf3
tarlepp <tarmo.leppanen@protacon.com>
2019-12-31 20:39:58 +0200

    feat: Added anonymous guard to login page.

--- src/app/auth/login/login.routes.ts
+++ src/app/auth/login/login.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login.component';
+import { AnonymousGuard } from '../guards';
 
 export const LoginRoutes: Routes = [
   {
     path: 'login',
+    canActivate: [
+      AnonymousGuard,
+    ],
     component: LoginComponent,
   },
 ];

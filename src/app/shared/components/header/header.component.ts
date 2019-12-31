37219e176655a88e771c5ccb6753e6f8676075ae
tarlepp <tarmo.leppanen@protacon.com>
2019-12-31 22:51:48 +0200

    feat: Implemented different menus for anon and logged in users + logout functionality.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { select, Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+
+import { authenticationActions, authenticationSelectors, AuthenticationState } from '../../../store/authentication';
+import { UserProfileInterface } from '../../../auth/interfaces';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  @ViewChild('userMenu', {static: true}) private userMenu: MatMenuTrigger;
+
+  public profile?: UserProfileInterface;
+  public loading$: Observable<boolean>;
+
+  private subscriptions: Subscription;
+
+  public constructor(private authenticationStore: Store<AuthenticationState>) {
+    this.subscriptions = new Subscription();
+  }
+
+  public ngOnInit(): void {
+    this.loading$ = this.authenticationStore.pipe(select(authenticationSelectors.loading));
+
+    this.subscriptions
+      .add(this.authenticationStore
+        .pipe(select(authenticationSelectors.profile))
+        .subscribe((profile: UserProfileInterface|null): void => {
+          this.profile = profile;
+        }),
+      );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  public logout(): void {
+    this.userMenu.closeMenu();
+    this.authenticationStore.dispatch(authenticationActions.logout());
+  }
+}

f976c2a66245103a1a2ffd11d6656fc4c19c4f32
tarlepp <tarmo.leppanen@protacon.com>
2020-01-05 21:28:45 +0200

    feat: Added missing selector for `roles` data to `Authentication` store.

--- src/app/store/authentication/authentication.selectors.ts
+++ src/app/store/authentication/authentication.selectors.ts
@@ -3,16 +3,19 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AuthenticationState } from './authentication.state';
 import { ServerErrorInterface } from '../../shared/interfaces';
 import { UserProfileInterface } from '../../auth/interfaces';
+import { Role } from '../../auth/enums';
 
 const authenticationState = createFeatureSelector<AuthenticationState>('authentication');
 const loading = createSelector(authenticationState, (state: AuthenticationState): boolean => state.loading);
 const loggedIn = createSelector(authenticationState, (state: AuthenticationState): boolean => state.loggedIn);
 const profile = createSelector(authenticationState, (state: AuthenticationState): UserProfileInterface => state.profile);
+const roles = createSelector(authenticationState, (state: AuthenticationState): Array<Role> => state.roles);
 const error = createSelector(authenticationState, (state: AuthenticationState): ServerErrorInterface|null => state.error);
 
 export const authenticationSelectors = {
   loading,
   loggedIn,
   profile,
+  roles,
   error,
 };

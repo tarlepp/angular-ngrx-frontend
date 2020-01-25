38edbe8636e9414c4ef5977477fc74ea521b2c84
tarlepp <tarmo.leppanen@protacon.com>
2020-01-25 19:27:02 +0200

    fix: Added missing return type

--- src/app/shared/factories/http-loader.factory.ts
+++ src/app/shared/factories/http-loader.factory.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function HttpLoaderFactory(httpClient: HttpClient) {
+export function HttpLoaderFactory(httpClient: HttpClient): TranslateHttpLoader {
   const ts = Math.round((new Date()).getTime() / 1000);
 
   return new TranslateHttpLoader(httpClient, './assets/i18n/', `.json?t=${ts}`);

35178d6aa9b164f3209c3f390e017eda0b9154d7
tarlepp <tarmo.leppanen@protacon.com>
2020-01-27 19:24:42 +0200

    refactor: `BaseGuard` should be abstract.

--- src/app/auth/guards/base.guard.ts
+++ src/app/auth/guards/base.guard.ts
@@ -4,7 +4,7 @@ import { take } from 'rxjs/operators';
 
 import { AuthenticationService } from '../services';
 
-export class BaseGuard {
+export abstract class BaseGuard {
   protected constructor(protected router: Router, protected authenticationService: AuthenticationService) { }
 
   protected makeCheck(needsToBeAuthenticated: boolean): Observable<boolean> {

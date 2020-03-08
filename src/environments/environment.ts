25b90d36046d323733bb05dc628d16776d990c47
tarlepp <tarmo.leppanen@protacon.com>
2020-03-08 14:54:54 +0200

    Added version information to `environment.ts` and `environment.prod.ts` files.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { version } from 'package.json';
+
 export const environment = {
   production: false,
   name: 'dev',
+  version,
 };
 
 /*

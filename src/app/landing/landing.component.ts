2e28cf7bfcb54b4e6dd03d786a8ea093e74fbfae
tarlepp <tarmo.leppanen@protacon.com>
2020-03-16 17:37:23 +0200

    clean up

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,11 +3,8 @@ import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 
 import { Device, Viewport } from 'src/app/shared/enums';
-import { LayoutState, VersionState } from 'src/app/store/store-states';
+import { LayoutState } from 'src/app/store/store-states';
 import { layoutSelectors } from 'src/app/store/store-selectors';
-import { HttpClient } from '@angular/common/http';
-import { ConfigurationService } from 'src/app/shared/services';
-import { versionActions } from 'src/app/store/version/version.actions';
 
 @Component({
   selector: 'app-landing',
@@ -21,7 +18,7 @@ export class LandingComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription;
 
-  constructor(private layoutStore: Store<LayoutState>, private http: HttpClient, private versionStore: Store<VersionState>) {
+  constructor(private layoutStore: Store<LayoutState>) {
     this.subscription = new Subscription();
   }
 
@@ -41,16 +38,5 @@ export class LandingComponent implements OnInit, OnDestroy {
 
   public ngOnDestroy(): void {
     this.subscription.unsubscribe();
-  }
-
-  public foo(): void {
-    this.http.get(ConfigurationService.configuration.apiUrl + '/healthz')
-      .subscribe((data) => {
-        console.log(data);
-      })
-  }
-
-  public bar(): void {
-    this.versionStore.dispatch(versionActions.fetchFrontendVersionSuccess({ version: Math.ceil(Math.random() * 10).toString() }))
   }
 }

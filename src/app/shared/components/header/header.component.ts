58caeb197116ccf4216c5ca9c7b62741ada75143
tarlepp <tarmo.leppanen@protacon.com>
2020-03-17 19:16:49 +0200

    Doc: Added missing documentation to `header.component`

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -10,12 +10,6 @@ import { AuthenticationState, LayoutState } from 'src/app/store/store-states';
 import { authenticationSelectors, layoutSelectors } from 'src/app/store/store-selectors';
 import { authenticationActions, layoutActions } from 'src/app/store/store-actions';
 
-// Note that if you add new language, you need to define it's text tag here
-marker([
-  'component.header.menu.language.en',
-  'component.header.menu.language.fi',
-]);
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -32,6 +26,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   private subscriptions: Subscription;
 
+  /**
+   * Constructor of the class, where we DI all services that we need to use
+   * within this component and initialize needed properties.
+   */
   public constructor(private authenticationStore: Store<AuthenticationState>, private layoutStore: Store<LayoutState>) {
     this.currentLanguage = Language.DEFAULT;
     this.subscriptions = new Subscription();
@@ -40,17 +38,32 @@ export class HeaderComponent implements OnInit, OnDestroy {
       .keys(Language)
       .filter((key: string): boolean => key !== 'DEFAULT')
       .map((key: string): Language => Language[key]);
+
+    // Note that if you add new language, you need to define it's text tag here
+    marker([
+      'component.header.menu.language.en',
+      'component.header.menu.language.fi',
+    ]);
   }
 
+  /**
+   * A callback method that is invoked immediately after the default change
+   * detector has checked the directive's data-bound properties for the first
+   * time, and before any of the view or content children have been checked.
+   * It is invoked only once when the directive is instantiated.
+   */
   public ngOnInit(): void {
+    // Loading state of `Authentication` store
     this.loading$ = this.authenticationStore.select(authenticationSelectors.loading);
 
+    // Subscribe to user profile changes
     this.subscriptions
       .add(this.authenticationStore
         .select(authenticationSelectors.profile)
         .subscribe((profile: UserProfileInterface|null): UserProfileInterface|null => this.profile = profile),
       );
 
+    // Subscribe to language changes
     this.subscriptions
       .add(this.layoutStore
         .select(layoutSelectors.language)
@@ -58,17 +71,27 @@ export class HeaderComponent implements OnInit, OnDestroy {
       );
   }
 
+  /**
+   * A callback method that performs custom clean-up, invoked immediately
+   * before a directive, pipe, or service instance is destroyed.
+   */
   public ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }
 
+  /**
+   * Method to dispatch logout action to authentication store, within this
+   * action store reducer + effect will make all necessary things to logout
+   * currently logged in user.
+   */
   public logout(): void {
     this.userMenu.closeMenu();
-    this.authenticationStore.dispatch(
-      authenticationActions.logout({ message: marker('messages.authentication.logout') }),
-    );
+    this.authenticationStore.dispatch(authenticationActions.logout({ message: marker('messages.authentication.logout') }));
   }
 
+  /**
+   * Method to dispatch change language action to layout store.
+   */
   public changeLanguage(language: Language): void {
     this.layoutStore.dispatch(layoutActions.changeLanguage({ language }));
   }

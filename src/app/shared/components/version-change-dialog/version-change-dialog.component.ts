ecac59d69c1bcc01d695b170d799d45d8ae30a97
tarlepp <tarmo.leppanen@protacon.com>
2020-03-17 19:45:17 +0200

    Doc: Added missing documentation to `version-change-dialog.component`

--- src/app/shared/components/version-change-dialog/version-change-dialog.component.ts
+++ src/app/shared/components/version-change-dialog/version-change-dialog.component.ts
@@ -9,6 +9,10 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class VersionChangeDialogComponent {
   public contentParams: { versionOld: string, versionNew: string };
 
+  /**
+   * Constructor of the class, where we DI all services that we need to use
+   * within this component and initialize needed properties.
+   */
   public constructor(
     private dialogRef: MatDialogRef<VersionChangeDialogComponent>,
     @Inject(MAT_DIALOG_DATA) private data: { versionOld: string, versionNew: string },

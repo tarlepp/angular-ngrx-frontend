8a0d9327eb681292f90b1f72b8f9ab2c1a667f71
tarlepp <tarmo.leppanen@protacon.com>
2020-03-17 20:11:58 +0200

    Doc: Added missing documentation to `AutoFocusDirective`

--- src/app/shared/directives/auto-focus.directive.ts
+++ src/app/shared/directives/auto-focus.directive.ts
@@ -3,16 +3,26 @@ import { Directive, Input, ElementRef, OnChanges, SimpleChanges } from '@angular
 @Directive({
   selector: '[appAutoFocus]',
 })
+
 export class AutoFocusDirective implements OnChanges {
   @Input() private appAutoFocus: boolean;
 
-  public constructor(private host: ElementRef) {
+  /**
+   * Constructor of the class, where we DI all services that we need to use
+   * within this component and initialize needed properties.
+   */
+  public constructor(private hostElement: ElementRef) {
     this.appAutoFocus = false;
   }
 
+  /**
+   * A callback method that is invoked immediately after the default change
+   * detector has checked data-bound properties if at least one has changed,
+   * and before the view and content children are checked.
+   */
   public ngOnChanges(changes: SimpleChanges): void {
     if (changes.appAutoFocus.currentValue === true) {
-      setTimeout((): void => this.host.nativeElement.focus(), 0);
+      setTimeout((): void => this.hostElement.nativeElement.focus(), 0);
     }
   }
 }

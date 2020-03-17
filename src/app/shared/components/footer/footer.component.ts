615e37889f349709854cd0b9b6a856dc5b722be6
Tarmo Leppänen <tarmo.leppanen@kase.fi>
2020-03-17 20:09:07 +0200

    Doc: Added missing documentation to shared components (#130)
    * Doc: Added missing documentation to `error-message.component`
    
    * Doc: Added missing documentation to `footer.component`
    
    * Doc: Added missing documentation to `header.component`
    
    * Doc: Added missing documentation to `version-change-dialog.component`

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -23,23 +23,42 @@ export class FooterComponent implements OnInit, OnDestroy, AfterViewInit {
 
   private subscriptions: Subscription;
 
+  /**
+   * Constructor of the class, where we DI all services that we need to use
+   * within this component and initialize needed properties.
+   */
   constructor(private versionStore: Store<VersionState>) {
     this.subscriptions = new Subscription();
   }
 
+  /**
+   * A callback method that is invoked immediately after the default change
+   * detector has checked the directive's data-bound properties for the first
+   * time, and before any of the view or content children have been checked.
+   * It is invoked only once when the directive is instantiated.
+   */
   public ngOnInit(): void {
+    // Initialize `versionFrontend$` and `versionBackend$` observables, that are used in component
     this.versionFrontend$ = this.versionStore.select(versionSelectors.versionFrontend);
     this.versionBackend$ = this.versionStore.select(versionSelectors.versionBackend);
 
+    // Lets check if frontend version has been changed every 5 minutes
     this.subscriptions
-      .add(interval(1000 * 60)
+      .add(interval(1000 * 60 * 5)
         .subscribe((): void => this.versionStore.dispatch(versionActions.fetchFrontendVersion())),
       );
 
+    // Fetch initial version of backend
     this.versionStore.dispatch(versionActions.fetchBackendVersion());
   }
 
+  /**
+   * A callback method that is invoked immediately after Angular has completed
+   * initialization of a component's view. It is invoked only once when the
+   * view is instantiated.
+   */
   public ngAfterViewInit(): void {
+    // We need to move our footer below the current viewport to get nice look
     setTimeout((): void => {
       const element = this.footerContainer.nativeElement;
 
@@ -48,6 +67,10 @@ export class FooterComponent implements OnInit, OnDestroy, AfterViewInit {
     }, 0);
   }
 
+  /**
+   * A callback method that performs custom clean-up, invoked immediately
+   * before a directive, pipe, or service instance is destroyed.
+   */
   public ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
   }

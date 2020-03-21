535e1f6f36085d708e2313566e67be4fcc0dd91c
tarlepp <tarmo.leppanen@protacon.com>
2020-03-21 21:19:03 +0200

    Feat: Added missing selector for RouterReducerState

--- src/app/store/store-selectors.ts
+++ src/app/store/store-selectors.ts
@@ -1,3 +1,4 @@
 export * from 'src/app/store/authentication/authentication.selectors';
 export * from 'src/app/store/layout/layout.selectors';
+export * from 'src/app/store/router/router.selectors';
 export * from 'src/app/store/version/version.selectors';

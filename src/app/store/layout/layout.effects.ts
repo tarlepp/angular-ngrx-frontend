b862f86bf83cc239fdb8929e1e8dc0ddd266eb55
tarlepp <tarmo.leppanen@protacon.com>
2020-03-25 20:28:11 +0200

    Doc: Added missing documentation to layout store.

--- src/app/store/layout/layout.effects.ts
+++ src/app/store/layout/layout.effects.ts
@@ -16,6 +16,16 @@ import { LayoutAction } from 'src/app/store/store.action';
 @Injectable()
 export class LayoutEffects {
   // noinspection JSUnusedLocalSymbols
+  /**
+   * NgRx effect for `LayoutAction.UPDATE_LOCALIZATION` action, within this
+   * we area actually switching this original action observable to multiple
+   * new action observables which are making following;
+   *  1) Change language.
+   *  2) Change locale.
+   *  3) Change timezone.
+   *
+   * Each of these actions you can find from this effect class.
+   */
   private changeLocalization$: Observable<TypedAction<LocalizationTypes>> = createEffect(
     (): Observable<TypedAction<LocalizationTypes>> => this.actions$
     .pipe(
@@ -30,6 +40,16 @@ export class LayoutEffects {
   );
 
   // noinspection JSUnusedLocalSymbols
+  /**
+   * NgRx effect for `LayoutAction.CHANGE_LANGUAGE` action, which will do
+   * following jobs;
+   *  1) Change the language within translate service, this will trigger
+   *     application to change all translations to match this new language.
+   *  2) Store new language to local storage, so that if user refresh the
+   *     page he/she will get the same language that he/she earlier chose.
+   *
+   * Within this effect we won't dispatch any other store actions.
+   */
   private changeLanguage$: Observable<void> = createEffect((): Observable<void> => this.actions$
       .pipe(
         ofType(LayoutAction.CHANGE_LANGUAGE),
@@ -43,6 +63,18 @@ export class LayoutEffects {
   );
 
   // noinspection JSUnusedLocalSymbols
+  /**
+   * NgRx effect for `LayoutAction.CHANGE_LOCALE` action, which will do
+   * following jobs;
+   *  1) Change moment.js library locale setting, this will affect to shown
+   *     time, date and datetime values in application that are formatted by
+   *     this library.
+   *  2) TODO:
+   *     Store new language to local storage, so that if user refresh the
+   *     page he/she will get the same locale that he/she earlier chose.
+   *
+   * Within this effect we won't dispatch any other store actions.
+   */
   private changeLocale$: Observable<void> = createEffect((): Observable<void> => this.actions$
     .pipe(
       ofType(LayoutAction.CHANGE_LOCALE),
@@ -53,6 +85,18 @@ export class LayoutEffects {
   );
 
   // noinspection JSUnusedLocalSymbols
+  /**
+   * NgRx effect for `LayoutAction.CHANGE_TIMEZONE` action, which will do
+   * following jobs;
+   *  1) Change moment.js library default timezone setting, this will affect
+   *     to shown time, date and datetime values in application that are
+   *     formatted by this library.
+   *  2) TODO:
+   *     Store new timezone to local storage, so that if user refresh the
+   *     page he/she will get the same timezone that he/she earlier chose.
+   *
+   * Within this effect we won't dispatch any other store actions.
+   */
   private changeTimezone$: Observable<void> = createEffect((): Observable<void> => this.actions$
     .pipe(
       ofType(LayoutAction.CHANGE_TIMEZONE),
@@ -63,6 +107,14 @@ export class LayoutEffects {
   );
 
   // noinspection JSUnusedLocalSymbols
+  /**
+   * NgRx effect for `LayoutAction.SCROLL_TO_TOP` action, purpose of this is to
+   * make user browser to scroll to top of the page.
+   *
+   * Within this event we just switch that original action observable to common
+   * `LayoutAction.SCROLL_TO` action which effect will actually do that scroll
+   * in browser.
+   */
   private scrollToTop$: Observable<TypedAction<LayoutAction.SCROLL_TO>> = createEffect(
     (): Observable<TypedAction<LayoutAction.SCROLL_TO>> => this.actions$
     .pipe(
@@ -72,6 +124,12 @@ export class LayoutEffects {
   );
 
   // noinspection JSUnusedLocalSymbols
+  /**
+   * NgRx effect for `LayoutAction.SCROLL_TO` action, purpose of this action is
+   * to scroll user browser to specified HTML anchor and after that switch that
+   * original action observable to `LayoutAction.CLEAR_SCROLL_TO` which clear
+   * that scroll to state in layout store.
+   */
   private scrollTo$: Observable<TypedAction<LayoutAction.CLEAR_SCROLL_TO>> = createEffect(
     (): Observable<TypedAction<LayoutAction.CLEAR_SCROLL_TO>> => this.actions$
     .pipe(
@@ -91,6 +149,10 @@ export class LayoutEffects {
     ),
   );
 
+  /**
+   * Constructor of the class, where we DI all services that we need to use
+   * within this component and initialize needed properties.
+   */
   public constructor(
     private actions$: Actions,
     private translateService: TranslateService,

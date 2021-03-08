fe6a7eaa4142191b10f9936e5ce5ea4add7b2285
tarlepp <tarmo.leppanen@protacon.com>
2021-03-08 16:31:22 +0200

    Renamed constant to match camelCase format

--- src/app/auth/login/login.routes.ts
+++ src/app/auth/login/login.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 import { AnonymousGuard } from 'src/app/auth/guards';
 import { LoginComponent } from 'src/app/auth/login/login.component';
 
-export const LoginRoutes: Routes = [
+export const loginRoutes: Routes = [
   {
     path: 'login',
     canActivate: [

d50bd3c10a046585bc4e66e79e49cbb35ec29942
tarlepp <tarmo.leppanen@protacon.com>
2021-04-03 13:55:28 +0300

    Added `appExcludedRole` and `appRequiredRole` directives to hide/show DOM elements by user role

--- src/app/shared/directives/index.ts
+++ src/app/shared/directives/index.ts
@@ -1,5 +1,9 @@
 import { AutoFocusDirective } from 'src/app/shared/directives/auto-focus.directive';
+import { ExcludedRoleDirective } from 'src/app/shared/directives/excluded-role.directive';
+import { RequiredRoleDirective } from 'src/app/shared/directives/required-role.directive';
 
 export const directives = [
   AutoFocusDirective,
+  ExcludedRoleDirective,
+  RequiredRoleDirective,
 ];
---
+++ src/app/shared/directives/required-role.directive.ts
@@ -0,0 +1,51 @@
+import { Directive, HostBinding, Input, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
+import { Role } from 'src/app/auth/enums';
+import { authenticationSelectors } from 'src/app/store';
+
+@Directive({
+  selector: '[appRequiredRole]',
+})
+
+export class RequiredRoleDirective implements OnInit, OnDestroy {
+  @Input('appRequiredRole') public role?: Role | string;
+
+  @HostBinding('style.display') private display: string;
+
+  private subscription: Subscription;
+
+  /**
+   * Constructor of the class, where we DI all services that we need to use
+   * within this component and initialize needed properties.
+   */
+  public constructor(private store: Store) {
+    this.display = 'none';
+    this.subscription = new Subscription();
+  }
+
+  /**
+   * A callback method that is invoked immediately after the default change
+   * detector has checked the directive's data-bound properties for the first
+   * time, and before any of the view or content children have been checked.
+   * It is invoked only once when the directive is instantiated.
+   */
+  public ngOnInit(): void {
+    this.subscription.add(
+      this.store.select(authenticationSelectors.hasRole(this.role)).subscribe(
+        (hasRole: boolean): void => {
+          this.display = hasRole ? '' : 'none';
+        },
+      ),
+    );
+  }
+
+  /**
+   * A callback method that performs custom clean-up, invoked immediately
+   * before a directive, pipe, or service instance is destroyed.
+   */
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}

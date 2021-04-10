fc15102199ba24beaef5feaefc8bb9d05359d2c3
tarlepp <tarmo.leppanen@protacon.com>
2021-04-10 19:08:06 +0300

    Fixed linter errors

--- src/app/shared/directives/index.ts
+++ src/app/shared/directives/index.ts
@@ -2,8 +2,8 @@ import { Type } from '@angular/core';
 
 import { AutoFocusDirective } from 'src/app/shared/directives/auto-focus.directive';
 import { ExcludedRoleDirective } from 'src/app/shared/directives/excluded-role.directive';
-import { RequiredRoleDirective } from 'src/app/shared/directives/required-role.directive';
 import { IsLoggedInDirective } from 'src/app/shared/directives/is-logged-in.directive';
+import { RequiredRoleDirective } from 'src/app/shared/directives/required-role.directive';
 
 export const directives: Array<Type<any>> = [
   AutoFocusDirective,

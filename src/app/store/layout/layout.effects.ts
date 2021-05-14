a5cbe1131353c186cf8740382fcc9ff37262ff1f
tarlepp <tarmo.leppanen@protacon.com>
2021-05-14 19:05:59 +0300

    Changed `withLatestFrom` to `concatLatestFrom`

--- src/app/store/layout/layout.effects.ts
+++ src/app/store/layout/layout.effects.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { TypedAction } from '@ngrx/store/src/models';
 import { TranslateService } from '@ngx-translate/core';
 import * as moment from 'moment-timezone';
 import { Observable } from 'rxjs';
-import { map, mergeMap, pluck, tap, withLatestFrom } from 'rxjs/operators';
+import { map, mergeMap, pluck, tap } from 'rxjs/operators';
 
 import { Language, Locale, Theme } from 'src/app/shared/enums';
 import { LocalizationInterface } from 'src/app/shared/interfaces';
@@ -153,7 +153,7 @@ export class LayoutEffects {
   private toggleThemeEffect$: Observable<TypedAction<LayoutType.SET_THEME>> = createEffect(
     (): Observable<TypedAction<LayoutType.SET_THEME>> => this.actions$.pipe(
       ofType(layoutActions.toggleTheme),
-      withLatestFrom(this.store.select(layoutSelectors.theme)),
+      concatLatestFrom((): Observable<Theme> => this.store.select(layoutSelectors.theme)),
       map(([, theme]: [any, Theme]): Theme => theme === Theme.DARK ? Theme.LIGHT : Theme.DARK),
       map((theme: Theme): TypedAction<LayoutType.SET_THEME> => layoutActions.setTheme({ theme })),
     ),

f88ef9829794326eeef9d9f8459c3049ceedc983
tarlepp <tarmo.leppanen@protacon.com>
2021-10-13 17:31:09 +0300

    Refactored `any` type usages to use proper type definitions

--- src/app/shared/services/snackbar-service.ts
+++ src/app/shared/services/snackbar-service.ts
@@ -9,6 +9,9 @@ import { ErrorMessageComponent } from 'src/app/shared/components';
 import { DictionaryInterface, ErrorMessageServerInterface, ServerErrorInterface } from 'src/app/shared/interfaces';
 import { errorActions } from 'src/app/store';
 
+type AppErrorMessageSnackBarType = MatSnackBarRef<SimpleSnackBar|ErrorMessageComponent>;
+type AppMessageSnackBarType = MatSnackBarRef<SimpleSnackBar>;
+
 @Injectable()
 export class SnackbarService {
   private closeButtonTag: string = marker('snackbar.close-button');
@@ -24,22 +27,24 @@ export class SnackbarService {
   ) { }
 
   /**
-   * Method to create plain snackbar message with specified content.
+   * Method to create plain snackbar message with specified content. MatSnackBarRef<TextOnlySnackBar>
    */
-  public message(message: string, duration: number = 6000): Promise<MatSnackBarRef<SimpleSnackBar>> {
-    return new Promise<MatSnackBarRef<SimpleSnackBar>>((resolve: any): void => {
-      const config: MatSnackBarConfig = {
-        duration,
-        panelClass: ['snackbar'],
-      };
+  public message(message: string, duration: number = 6000): Promise<AppMessageSnackBarType> {
+    return new Promise<AppMessageSnackBarType>(
+      (resolve: (value: AppMessageSnackBarType|PromiseLike<AppMessageSnackBarType>) => void): void => {
+        const config: MatSnackBarConfig = {
+          duration,
+          panelClass: ['snackbar'],
+        };
 
-      this.translateService
-        .get([message, this.closeButtonTag])
-        .pipe(take(1))
-        .subscribe((texts: DictionaryInterface<string>): void =>
-          resolve(this.snackBar.open(texts[message], texts[this.closeButtonTag], config)),
-        );
-    });
+        this.translateService
+          .get([message, this.closeButtonTag])
+          .pipe(take(1))
+          .subscribe((texts: DictionaryInterface<string>): void =>
+            resolve(this.snackBar.open(texts[message], texts[this.closeButtonTag], config)),
+          );
+      },
+    );
   }
 
   /**
@@ -48,40 +53,42 @@ export class SnackbarService {
    * contains data that can be shown in that separated component - these errors
    * are usually backend validation related errors.
    */
-  public error(error: ServerErrorInterface): Promise<MatSnackBarRef<SimpleSnackBar|ErrorMessageComponent>> {
-    return new Promise<MatSnackBarRef<SimpleSnackBar>>((resolve: any): void => {
-      const config: MatSnackBarConfig = {
-        panelClass: ['snackbar', 'snackbar--error'],
-      };
+  public error(error: ServerErrorInterface): Promise<AppErrorMessageSnackBarType> {
+    return new Promise<AppErrorMessageSnackBarType>(
+      (resolve: (value: AppErrorMessageSnackBarType|PromiseLike<AppErrorMessageSnackBarType>) => void): void => {
+        const config: MatSnackBarConfig = {
+          panelClass: ['snackbar', 'snackbar--error'],
+        };
 
-      let serverErrorMessages: Array<ErrorMessageServerInterface> = [];
+        let serverErrorMessages: Array<ErrorMessageServerInterface> = [];
 
-      try {
-        serverErrorMessages = JSON.parse(error.message);
+        try {
+          serverErrorMessages = JSON.parse(error.message);
 
-        if (!Array.isArray(serverErrorMessages)) {
-          serverErrorMessages = [];
-        }
-      } catch (e) { }
-
-      this.translateService
-        .get(this.closeButtonTag)
-        .pipe(take(1))
-        .subscribe((closeButton: string): void => {
-          let matSnackBarRef;
-
-          if (serverErrorMessages.length > 0) {
-            matSnackBarRef = this.snackBar.openFromComponent(ErrorMessageComponent, { ...config, data: serverErrorMessages });
-          } else {
-            matSnackBarRef = this.snackBar.open(error.message, closeButton, config);
+          if (!Array.isArray(serverErrorMessages)) {
+            serverErrorMessages = [];
           }
+        } catch (e) { }
 
-          matSnackBarRef
-            .afterDismissed()
-            .subscribe((): void => this.store.dispatch(errorActions.clearSnackbar()));
+        this.translateService
+          .get(this.closeButtonTag)
+          .pipe(take(1))
+          .subscribe((closeButton: string): void => {
+            let matSnackBarRef;
 
-          resolve(matSnackBarRef);
-        });
-    });
+            if (serverErrorMessages.length > 0) {
+              matSnackBarRef = this.snackBar.openFromComponent(ErrorMessageComponent, { ...config, data: serverErrorMessages });
+            } else {
+              matSnackBarRef = this.snackBar.open(error.message, closeButton, config);
+            }
+
+            matSnackBarRef
+              .afterDismissed()
+              .subscribe((): void => this.store.dispatch(errorActions.clearSnackbar()));
+
+            resolve(matSnackBarRef);
+          });
+      },
+    );
   }
 }

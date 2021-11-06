da110723bd621de2116002ca47dfc8d60ac8feb2
tarlepp <tarmo.leppanen@protacon.com>
2021-11-06 12:12:38 +0200

    Refactored use of `any` type definitions out of the ngrx helper utils

--- src/app/shared/utils/ngrx.utils.ts
+++ src/app/shared/utils/ngrx.utils.ts
@@ -1,7 +1,6 @@
 import { createSelector, MemoizedSelector } from '@ngrx/store';
 
 import { ServerErrorInterface } from 'src/app/shared/interfaces';
-import { IsLoadingAwareState } from 'src/app/store/aware-states';
 
 /**
  * This file contains set of helpers for NgRX usage.
@@ -9,30 +8,30 @@ import { IsLoadingAwareState } from 'src/app/store/aware-states';
  * If you spot out some generic (and repeating) code in this application that
  * is related to NgRx stuff please refactor those parts and add new helper(s)
  * here.
- *
- * TODO try to avoid that `any` type with these...
  */
 
 // Helper selector for any boolean value from specified feature state
 export const selectBooleanValue =
-  (selector: MemoizedSelector<any, any>, property: string): MemoizedSelector<any, boolean> =>
-    createSelector(selector, (state: IsLoadingAwareState|any): boolean => getValue<boolean>(state, property));
+  <T1, T2>(selector: MemoizedSelector<T1, T2>, property: string): MemoizedSelector<T1, boolean> =>
+    createSelector(selector, (state: T2): boolean => getValue<T2, boolean>(state, property));
 
 // Helper selector for any string value from specified feature state
 export const selectStringValue =
-  (selector: MemoizedSelector<any, any>, property: string): MemoizedSelector<any, string> =>
-    createSelector(selector, (state: any): string => getValue<string>(state, property));
+  <T1, T2>(selector: MemoizedSelector<T1, T2>, property: string): MemoizedSelector<T1, string> =>
+    createSelector(selector, (state: T2): string => getValue<T2, string>(state, property));
 
 // Helper selector for `ServerErrorInterface` value from specified feature state
 export const selectServerErrorValue =
-  (selector: MemoizedSelector<any, any>, property: string): MemoizedSelector<any, ServerErrorInterface|null> =>
-    createSelector(selector, (state: any): ServerErrorInterface|null =>
-      getValue<ServerErrorInterface|null>(state, property));
+  <T1, T2>(selector: MemoizedSelector<T1, T2>, property: string): MemoizedSelector<T1, ServerErrorInterface|null> =>
+    createSelector(selector, (state: T2): ServerErrorInterface|null =>
+      getValue<T2, ServerErrorInterface|null>(state, property));
 
-const getValue = <T>(state: any, property: string): T => {
+const getValue = <T1, T2>(state: T1, property: string): T2 => {
   if (!Object.keys(state).includes(property)) {
     throw new Error(`Property '${property}' is not defined in state ${JSON.stringify(state)}`);
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return state[property];
 };

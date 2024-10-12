82568fae9bb7a763d1fcf5a79db7a01ab47f829d
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2024-10-13 01:58:29 +0300

    Fixed import order

--- src/app/shared/components/error-message/error-message.component.ts
+++ src/app/shared/components/error-message/error-message.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
-import { marker } from '@jsverse/transloco-keys-manager/marker';
 import { TranslocoService } from '@jsverse/transloco';
+import { marker } from '@jsverse/transloco-keys-manager/marker';
 import { take } from 'rxjs/operators';
 
 import {

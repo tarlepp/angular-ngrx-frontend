01f14aecfdfa2b07f3d3ceb1f32bbe8c02f77886
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2025-09-12 18:02:25 +0300

    Refactored guards to use simpler logic

--- src/app/auth/guards/anonymous.guard.ts
+++ src/app/auth/guards/anonymous.guard.ts
@@ -1,28 +1,13 @@
-import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { BaseAuth } from 'src/app/auth/guards/base-auth';
-import { AuthenticationService } from 'src/app/auth/services';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnonymousGuard extends BaseAuth {
-  protected readonly router: Router = inject(Router);
-  protected readonly authenticationService: AuthenticationService = inject(AuthenticationService);
-
-  /**
-   * Constructor of the class, where we DI all services that we need to use
-   * within this guard.
-   */
-  public constructor() {
-    const router: Router = inject(Router);
-    const authenticationService: AuthenticationService = inject(AuthenticationService);
-
-    super(router, authenticationService);
-  }
-
   /**
    * Purpose of this guard is check that current user has not been authenticated
    * to application. If user is authenticated he/she is redirected to application
